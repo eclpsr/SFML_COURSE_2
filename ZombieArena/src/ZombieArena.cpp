727beeae62daac5a18d53b3361a8a612beeb64a6
Artem Reshetnikov <balupwns@gmail.com>
2021-03-26 16:40:35 +0900

    37. Starting the Zombie Arena Game Engine

--- ZombieArena/src/ZombieArena.cpp
+++ ZombieArena/src/ZombieArena.cpp
@@ -1,23 +1,47 @@
 #include <SFML/Graphics.hpp>
+#include "Player.h"
+
+using namespace sf;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	// The game will always be in one of four states
+	enum class State{
+		PAUSED, LEVELING_UP, GAME_OVER, PLAYING
+	};
+	// Start with the GAME_OVER state
+	State state = State::GAME_OVER;
+	// Get the screen resolution and create an SMFL window
+	Vector2f resolution;
+	resolution.x = VideoMode::getDesktopMode().width;
+	resolution.y = VideoMode::getDesktopMode().height;
 
+	RenderWindow window(VideoMode(resolution.x, resolution.y),
+			"Zombie Arena", Style::Fullscreen);
+
+	// Create a an SFML View for the main action
+	View mainView(sf::FloatRect(0, 0, resolution.x, resolution.y));
+
+	// Here is our clock for timing everything
+	Clock clock;
+	// How long has the PLAYING state been active
+	Time gameTimeTotal;
+
+	// Where is the mouse in relation to world coordinates
+	Vector2f mouseWorldPosition;
+	// Where is the mouse in relation to screen coordinates
+	Vector2i mouseScreenPosition;
+
+	// Create an instance of the Player class
+	Player player;
+
+	// The boundaries of the arena
+	IntRect arena;
+
+	// The main game loop
     while (window.isOpen())
     {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
 
-        window.clear();
-        window.draw(shape);
-        window.display();
     }
 
     return 0;
